c0be7f9dfe8625355758e0b6b4655001812d13de
solutionman <solutionman@inbox.ru>
2022-11-05 16:18:24 +0700

    save Person

--- src/main/java/com/phone/contacts/RestController.java
+++ src/main/java/com/phone/contacts/RestController.java
@@ -2,6 +2,7 @@ package com.phone.contacts;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.HashMap;
 import java.util.List;
@@ -10,7 +11,8 @@ import java.util.Map;
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
     private final PersonRepository personRepository;
-    public RestController(PersonRepository personRepository){
+
+    public RestController(PersonRepository personRepository) {
         this.personRepository = personRepository;
     }
 
@@ -26,8 +28,20 @@ public class RestController {
     }
 
     @PostMapping("/create")
-    public Map<String, String> create(Person person) {
-        personRepository.save(person);
+    public Object create(@RequestBody Map<String, Object> data) {
+        try {
+            Map<String, Object> persons = (Map<String, Object>) data.get("data");
+            if (persons.get("name") != null) {
+                Person person = new Person();
+                person.setName(persons.get("name").toString());
+                person.setFamily(persons.get("family").toString());
+                person.setPhone(persons.get("phone").toString());
+                personRepository.save(person);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         Map<String, String> result = new HashMap<>();
         result.put("message", "created");
         return result;
